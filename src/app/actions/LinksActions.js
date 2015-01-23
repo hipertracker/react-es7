0b9fdf65be0e012d96aa78a500e08f26901c9d94
Jaroslaw Zabiello <hipertracker@gmail.com>
2015-01-23 01:52:52 +0000

    Update LinksActions.js

--- src/app/actions/LinksActions.js
+++ src/app/actions/LinksActions.js
@@ -1,6 +1,5 @@
 import * as Reflux from 'reflux';
 
-
 // Each action is like an event channel for one specific event. Actions are called by components.
 // The store is listening to all actions, and the components in turn are listening to the store.
 // Thus the flow is: User interaction -> component calls action -> store reacts and triggers -> components update
